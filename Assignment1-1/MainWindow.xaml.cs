3a0ec8bec2dc8ce1780c5bf2558b10377c178f56
Korbin Dansie <dansiekm@gmail.com>
2023-01-14 14:59:06 -0700

    Add project files.

---
+++ Assignment1-1/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment1_KorbinDansie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Display a message box when the top button is clicked
+        /// Also display which button in the message box was clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnOne_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Your clicked the top button\nYou typed: " + TextBoxOne.Text, "Button One", MessageBoxButton.OK, MessageBoxImage.Error);
+            LbResult.Content = $"top button's {result} button";
+            SetVisablityForResults();
+        }
+
+        /// <summary>
+        /// Display a message box when the middle button is clicked
+        /// Also display which button in the message box was clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnTwo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Your clicked the middle button\nYou typed: " + TextBoxTwo.Text, "Button Two", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+            LbResult.Content = $"middle button's {result} button";
+            SetVisablityForResults();
+        }
+
+        /// <summary>
+        /// Display a message box when the bottom button is clicked
+        /// Also display which button in the message box was clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnThree_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Your clicked the bottom button\nYou typed: " + TextBoxThree.Text, "Button Three", MessageBoxButton.YesNo, MessageBoxImage.Information);
+            LbResult.Content = $"bottom button's {result} button";
+            SetVisablityForResults();
+        }
+
+        /// <summary>
+        /// Set the result sections visiblity to Visible 
+        /// </summary>
+        private void SetVisablityForResults()
+        {
+            LbResultIntroText.Visibility = Visibility.Visible;
+            LbResult.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ Assignment1-2/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Assignment 1 - Part 2");
+Console.WriteLine("By: Korbin Dansie");
+
+// Start of the program
+int firstNumber;
+int secondNumber;
+string input;
+
+Console.WriteLine();
+
+// Get the first number
+do
+{
+    Console.Write("Please enter the first number: ");
+    input = Console.ReadLine();
+} while (!Int32.TryParse(input, out firstNumber));
+
+// Get the second number
+do
+{
+    Console.Write("Please enter the second number: ");
+    input = Console.ReadLine();
+} while (!Int32.TryParse(input, out secondNumber));
+
+// Display the results
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
+
+Console.WriteLine();
+Console.WriteLine();
+
+// Display the comparing results
+// less than
+if (firstNumber < secondNumber)
+{
+    Console.WriteLine($"{firstNumber} is less than {secondNumber}");
+}
+else
+{
+    Console.WriteLine($"{firstNumber} is not less than {secondNumber}");
+}
+
+// greater than
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine($"{firstNumber} is greater than {secondNumber}");
+}
+else
+{
+    Console.WriteLine($"{firstNumber} is not greater than {secondNumber}");
+}
+
+// equals
+if (firstNumber == secondNumber)
+{
+    Console.WriteLine($"{firstNumber} is equal to {secondNumber}");
+}
+else
+{
+    Console.WriteLine($"{firstNumber} does not equal {secondNumber}");
+}
+
+
+//Pause before ending
+Console.ReadLine();
